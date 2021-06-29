e109699a9506d743cce0c3d529d34ee4d7ae7241
Park Hyung Ki <pretty_gm@naver.com>
2021-06-29 18:26:13 +0900

    login 기능 만들기

---
+++ part7_security/src/main/java/www/dream/com/party/model/Manager.java
@@ -0,0 +1,62 @@
+package www.dream.com.party.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 시스템 운영자
+ * @author Park
+ */
+@Data
+@NoArgsConstructor
+public class Manager extends Party {public Manager(String userId) {
+		super(userId);
+	} // 7-1. Party를 상속받는 Manager 클래스 생성
+	
+private static List<AuthorityVO> listAuthority = new ArrayList<>();
+	static {
+		listAuthority.add(new AuthorityVO("manager"));
+		listAuthority.add(new AuthorityVO("user"));
+	}
+	@Override
+	public List<AuthorityVO> getAuthorityList() {
+		return listAuthority;
+	}
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
